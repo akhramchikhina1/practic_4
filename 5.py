5b5304ee86e094274b80ec756b0f606c783b3b46
akhramchikhina1 <a.khramchikhina1@g.nsu.ru>
2024-03-02 17:53:30 +0700

    print 5

---
+++ 5.py
@@ -0,0 +1,7 @@
+print('Как зовут главных героев игры Maincraft? ')
+hero_first=input('Имя первого героя: ')
+hero_second=input('Имя второго героя: ')
+if hero_first==('') and hero_second==(''):
+    print('Верно')
+else:
+    print('Неверно')
