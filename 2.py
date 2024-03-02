64cb332a799d7d03e5b1b96e3caf61e074247b5c
akhramchikhina1 <a.khramchikhina1@g.nsu.ru>
2024-03-02 18:40:08 +0700

    print 2

--- 2.py
+++ 2.py
@@ -1,8 +1,9 @@
 print('Какие два слова передал своей радиограммой Александр Попов?')
 first_word=input('Первое слово: ')
 second_word=input('Второе слово: ')
-if first_word == 'добрый':
-    if second_word == 'день!':
+if first_word == 'это':
+    if second_word == 'попова':
         print('Верно')
 else:
     print('Неверно')
+ #полуж
