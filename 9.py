c0aa51a6016518e1035c2874234c3c4f25a4c749
akhramchikhina1 <a.khramchikhina1@g.nsu.ru>
2024-03-16 11:52:50 +0700

    print 9

--- 9.py
+++ 9.py
@@ -1,7 +1,49 @@
-name=input('Здравствуйте! Как Вас зовут? ')
-print(f'Очень приятно, {name}! Меня зовут Марк.')
-years=int(input('Сколько Вам лет? '))
-if years>78:
-    print(f'Да, {name}, Вы старше меня на {years-78} лет')
+poroda=input('Собака короткошерстной породы? ')
+if poroda=='да':
+    high=input('Рост собаки менее 50 см? ')
+    if high == 'да':
+        tail=input('У собаки короткий хвост? ')
+        if tail == 'да':
+            print('английский бульдог')
+        else:
+            ears=input('У собаки длинные уши?')
+            if ears== 'да':
+                print('гончая')
+            else:
+                body=('У собаки короткое тело? ')
+                if body=='да':
+                    print('мопс')
+                else:
+                    print('чихуахуа')
+    else:
+        weight=input('Собака весит более 50 кг?' )
+        if weight=='да':
+            print('датский дог')
+        else:
+            print('фоксхаунд')
 else:
-    print(f'Да, {name}, я старше Вас на {78-years} лет')
+    high = input('Рост собаки менее 50 см? ')
+    if high == 'да':
+        character = input('У собаки доброжелательный характер? ')
+        if character == 'да':
+            print('кокер-спаниэль')
+        else:
+            print('ирландский сеттер')
+    else:
+        high_second=input('У собаки менее 70 см? ')
+        if high_second=='да':
+            ears = input('У собаки длинные уши?')
+            if ears == 'да':
+                print('большой вандейский грифон')
+            else:
+                print('колли')
+        else:
+            color=input('Окрас рыжий с белыми отметинами? ')
+            if color=='да':
+                print('сенбернар')
+            else:
+                while_color=input('Белоснежный окрас? ')
+                if while_color == 'да':
+                    print('ирландский волкодав')
+                else:
+                    print('ньюфаундленд')
